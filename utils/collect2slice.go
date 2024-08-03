9a8f18e73ef80fe6c5af212bdc5970dad4e0ce49
BaiMeow <baishu2003@outlook.com>
2024-08-03 15:46:25 +0800

    fix: collect

--- utils/collect2slice.go
+++ utils/collect2slice.go
@@ -3,21 +3,25 @@ package utils
 type Collector[T any] struct {
 	C     chan T
 	slice []T
+	done  chan struct{}
 }
 
 func NewCollector[T any]() *Collector[T] {
 	c := &Collector[T]{
-		C: make(chan T),
+		C:    make(chan T, 16),
+		done: make(chan struct{}),
 	}
 	go func() {
 		for v := range c.C {
 			c.slice = append(c.slice, v)
 		}
+		c.done <- struct{}{}
 	}()
 	return c
 }
 
 func (c *Collector[T]) Return() []T {
 	close(c.C)
+	<-c.done
 	return c.slice
 }
